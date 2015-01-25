66751b011bdebbfb3e245d60ec2f7e37918da256
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2015-01-25 14:06:59 +0100

    Remap tmux prefix to <C-a>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
