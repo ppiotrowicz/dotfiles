cd627dc95a39e5f3fa89fff536d7363d7930840e
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-06-19 20:05:41 +0200

    Fix spacing issue in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,13 +9,13 @@ zstyle ':vcs_info:*' enable git svn
 
 precmd () {
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-        zstyle ':vcs_info:*' formats '%c%u'
+        zstyle ':vcs_info:*' formats ' %c%u'
     } else {
-        zstyle ':vcs_info:*' formats '%c%u%F{yellow}✱'
+        zstyle ':vcs_info:*' formats ' %c%u%F{yellow}✱'
     }
 
     vcs_info
 }
 
 setopt prompt_subst
-PROMPT='%F{white} λ %F{blue}%~ ${vcs_info_msg_0_}%F{blue} ❯ %{$reset_color%}'
+PROMPT='%F{white} λ %F{blue}%~${vcs_info_msg_0_}%F{blue} ❯ %{$reset_color%}'
