71499f952ab9522e03c0230031aa37d2164b86c4
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-07-14 23:56:12 +0200

    auto gpg setup

--- setup_scripts/setup_configs.sh
+++ setup_scripts/setup_configs.sh
@@ -20,3 +20,8 @@ ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
 # ag
 ln -s ~/dotfiles/agignore ~/.agignore
+
+# gpg
+mkdir ~/.gnupg
+ln -s ~/dotfiles/gpg/gpg.conf ~/.gnupg/
+ln -s ~/dotfiles/gpg/gpg-agent.conf ~/.gnupg/
