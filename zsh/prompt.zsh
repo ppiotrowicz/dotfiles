272ab00463a015922c227a90c2b5dbe264175f5b
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2017-09-16 13:28:19 +0200

    [zsh] update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,15 +3,15 @@ autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' stagedstr '%F{green}✱'
 zstyle ':vcs_info:*' unstagedstr '%F{red}✱'
-zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' check-for-changes false
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
-zstyle ':vcs_info:*' enable git svn
+zstyle ':vcs_info:*' enable git
 
 precmd () {
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
         zstyle ':vcs_info:*' formats ' %F{green}%b %c%u%f'
     } else {
-        zstyle ':vcs_info:*' formats ' %F{green}%b %c%u%F{yellow}✱%f (%a)'
+        zstyle ':vcs_info:*' formats ' %F{green}%b %c%u%F{yellow}✱%f'
     }
 
     vcs_info
