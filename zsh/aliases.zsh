8fe126080ea3f474cb2866a057a636ae346688a2
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2018-04-22 23:13:37 +0200

    New aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,8 @@ alias test="bundle exec rspec spec"
 # rails
 alias migrate="bundle exec rake db:migrate"
 alias rollback="bundle exec rake db:rollback"
+
+# exa
+alias ls=exa
+
+alias shuttle-cli="RBENV_VERSION=2.4.2 shuttle-cli"
